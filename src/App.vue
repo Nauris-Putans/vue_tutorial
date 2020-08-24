1911682792e4c461785d6542212f9f7270b2b259
Nauris <nauris1122@gmail.com>
2020-08-24 20:24:20 +0300

    Added showing correct/inccorect answer

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
-    <Header />
+    <Header
+      :numCorrect="numCorrect"
+      :numTotal="numTotal"
+    />
 
     <b-container class="bv-example-row">
       <b-row>
@@ -9,6 +12,7 @@
             v-if="questions.length"
             :currentQuestion="questions[index]"
             :next="next"
+            :increment="increment"
           />
         </b-col>
       </b-row>
@@ -29,13 +33,21 @@ export default {
   data: function() {
     return {
       questions: [],
-      index: 0
+      index: 0,
+      numCorrect: 0,
+      numTotal: 0
     }
   },
   methods: {
     // if next() method is called the index will be added up by +1
     next: function() {
       this.index++
+    },
+    increment(isCorrect) {
+      if (isCorrect) {
+        this.numCorrect++
+      }
+      this.numTotal++
     }
   },
   mounted: function() {
--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,3 +1,4 @@
+<!--suppress JSUnresolvedVariable, JSUnresolvedFunction -->
 <template>
     <div class="question-box-container">
         <b-jumbotron>
@@ -12,13 +13,20 @@
                     v-for="(answer, index) in answers"
                     :key="index"
                     @click="selectAnswer(index)"
+                    :class="answerClass(index)"
                 >
                     {{ answer }}
                 </b-list-group-item>
             </b-list-group>
 
-            <b-button variant="primary" href="#">Submit</b-button>
-            <b-button @click="next" variant="success" href="#">
+            <b-button
+                variant="primary"
+                @click="submitAnswer"
+                :disabled="selectedIndex === null || answered"
+            >
+                Submit
+            </b-button>
+            <b-button @click="next" variant="success">
                 Next
             </b-button>
         </b-jumbotron>
@@ -26,14 +34,20 @@
 </template>
 
 <script>
+    import _ from 'lodash'
+
     export default {
         props: {
             currentQuestion: Object,
-            next: Function
+            next: Function,
+            increment: Function
         },
         data() {
             return {
-                selectedIndex: null
+                selectedIndex: null,
+                correctIndex: null,
+                shuffledAnswers: [],
+                answered: false
             }
         },
         computed: {
@@ -43,10 +57,50 @@
                 return answers
             }
         },
+        watch: {
+            currentQuestion: {
+                immediate: true,
+                handler() {
+                    this.selectedIndex = null
+                    this.answered = false
+                    this.shuffleAnswers()
+                }
+            }
+        },
         methods: {
             selectAnswer(index) {
                 this.selectedIndex = index
-                console.log(index)
+            },
+            submitAnswer() {
+              let isCorrect = false
+
+              if (this.selectedIndex === this.correctIndex) {
+                  isCorrect = true
+              }
+
+              this.answered = true
+
+              this.increment(isCorrect)
+
+            },
+            shuffleAnswers() {
+                let answers = [...this.currentQuestion.incorrect_answers, this.currentQuestion.correct_answer]
+                this.shuffledAnswers = _.shuffle(answers)
+                this.correctIndex = this.shuffledAnswers.indexOf(this.currentQuestion.correct_answer)
+            },
+            answerClass(index) {
+                let answerClass = ''
+
+                if (!this.answered && this.selectedIndex === index) {
+                    answerClass = 'selected'
+                } else if (this.answered && this.correctIndex === index) {
+                    answerClass = 'correct'
+                } else if (this.answered && this.selectedIndex === index && this.correctIndex !== index) {
+                    answerClass = 'incorrect'
+                }
+
+                return answerClass
+
             }
         }
     }
@@ -56,7 +110,25 @@
     .list-group {
         margin-bottom: 15px;
     }
+
+    .list-group-item:hover {
+        background: #EEE;
+        cursor: pointer;
+    }
+
     .btn {
         margin: 0 5px;
+    }
+
+    .selected {
+        background-color: lightblue;
+    }
+
+    .correct {
+        background-color: lightgreen;
+    }
+
+    .incorrect {
+        background-color: red;
     }
 </style>
